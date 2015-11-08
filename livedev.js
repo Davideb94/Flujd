3c495e1fcddd35f9d63cb2add43cde833d3406e9
Davide Brunetti <davide.brunetti@novetica.org>
2015-11-08 20:06:43 +0000

    Setting the watcher on the current path

--- livedev.js
+++ livedev.js
@@ -1,32 +1,28 @@
-var fs = require('fs');		//Includes File System to use fs.watch
-var express = require('express');	//Includes Express to start an express server
-var app = express();	//Creates an instance of express
-	app.use(express.static(__dirname + '/public'));		//Selects the directory where the server has to watch
-var server = require('http').Server(app);	//Includes the http module and creates the server
-var io = require('socket.io')(server);	//Includes socket.io to communicate between the server and the browser
-var open = require('open');		//Includes node-open (https://github.com/pwnall/node-open) to automatically open a browser's window
-
 //Defines which page to read. Default: index.html
 var source = 'index.html';
 if(process.argv[2]!=null)
 	source = process.argv[2];
 //--end
 
-//Defines which folder to watch. Default: public
-var toWatch = 'public';
-if(process.argv[3]!=null)
-	toWatch = process.argv[3];
-//--end
+var toWatch = './';	//Defines which folder to watch.
 
 //Defines which port to use. Default: 8888
 var port = 8888;
-if(process.argv[4]!=null)
-	port = process.argv[4];
+if(process.argv[3]!=null)
+	port = process.argv[3];
 //--end
+
+var fs = require('fs');		//Includes File System to use fs.watch
+var express = require('express');	//Includes Express to start an express server
+var app = express();	//Creates an instance of express
+	app.use(express.static(__dirname + '/' + toWatch));		//Selects the directory where the server has to watch
+var server = require('http').Server(app);	//Includes the http module and creates the server
+var io = require('socket.io')(server);	//Includes socket.io to communicate between the server and the browser
+var open = require('open');		//Includes node-open (https://github.com/pwnall/node-open) to automatically open a browser's window
 
 //Defines what to do when the port is pinged
 app.get('/', function(req,res){
-	res.render('/' + toWatch + '/' +source);
+	res.render('/' + toWatch + source);
 });
 //--end
 
